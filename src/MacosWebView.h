d2e24032771c9a9ac2ea7e0c8e1bbefa931baa69
zhuzichu <zhuzichu520@outlook.com>
2024-09-13 20:07:26 +0800

    update

--- src/MacosWebView.h
+++ src/MacosWebView.h
@@ -2,22 +2,18 @@
 
 #include <QSharedPointer>
 #include <QWindow>
-#include <WebView2.h>
 #include <qjsvalue.h>
-#include <windows.h>
 #include <QMap>
-#include <wrl.h>
 
 #include "WebCallBack.h"
 #include "WebViewInterface.h"
 
-using namespace Microsoft::WRL;
 
-class WindowsWebView : public WebViewInterface {
+class MacosWebView : public WebViewInterface {
 
 public:
-    WindowsWebView();
-    ~WindowsWebView();
+    MacosWebView();
+    ~MacosWebView();
     void init(bool debug, QWindow *window, WebCallBack *callBack) override;
     void navigate(const QString &url);
     void resizeWebView();
@@ -25,15 +21,9 @@ public:
     void runJavaScript(const QString &js);
 
 private:
-    HRESULT OnEnvironmentCreated(HRESULT result, ICoreWebView2Environment *env, bool debug);
-
-private:
-    ComPtr<ICoreWebView2Controller> m_webViewController;
-    ComPtr<ICoreWebView2> m_webView;
     QWindow *m_childWindow;
     QWindow *m_window;
     WebCallBack *m_callBack;
-    HWND m_childWindowHandle;
     QString m_userDataFolder;
     QMap<QString, QJSValue> m_bindings;
 };
